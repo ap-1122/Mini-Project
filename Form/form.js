3effaab389902559127640d2ee24ded5a8255fc9
ap-1122 <apsingh231306@gmail.com>
2025-07-23 11:38:46 +0530

    commit msg

---
+++ Form/form.js
@@ -0,0 +1,68 @@
+let formvalid=()=>{
+    let Name=document.querySelector("#name").value.trim();
+    let Phone=document.querySelector("#phoneno").value.trim();
+    let Email=document.querySelector("#email").value.trim();
+    let Address=document.querySelector("#address").value.trim();
+    let Pincode=document.querySelector("#pincode").value.trim();
+    let State=document.querySelector("#state").value.trim();
+    let PW=document.querySelector("#pass").value.trim();
+    let  Cfpw =document.querySelector("#cfpw").value.trim();
+
+    let errorname=document.querySelector("#errorname")
+    let errorphone=document.querySelector("#errorphone")
+    let erroremail=document.querySelector("#erroremail")
+    let erroraddress=document.querySelector("#erroraddress")
+    let errorpin=document.querySelector("#errorpincode")
+    let errorstate=document.querySelector("#errorstate")
+    let errorpw=document.querySelector("#errorpass")
+    let cfpwpara=document.querySelector("#errorcfpw")
+       let notmatch=document.querySelector("#notmatch")
+
+
+    if(Name==""){
+        errorname.innerHTML="Please fill your name"
+        document.querySelector("#name").style.border="3px solid red"
+        return false;
+    }
+   else if(Phone.length!=10){
+        errorphone.innerHTML="Please enter your Phone No"
+        return false;
+   }
+    else if(!(Email.includes('@') && Email.includes('.com')) ){
+        erroremail.innerHTML="Please enter Correct Email "
+        return false;
+   }
+   else if(Address==""){
+        erroraddress.innerHTML="Please enter address No"
+        return false;
+   }
+//    else if(Pincode.length!=5){
+//         errorpin.innerHTML="Please enter your correct pincode No"
+//         return false;
+//    }
+//    else if(Phone.length!=10){
+//         errorphone.innerHTML="Please enter your Phone No"
+//         return false;
+//    }
+
+       else if(!(PW.match(/[!@#$%^&*]/) &&
+              PW.match(/[1234567890]/)   &&
+              PW.match(/[A-Z]/)   && 
+              PW.match(/[a-z]/)   )){
+
+        errorpw.innerHTML="Enter Strong Password"
+        return false;
+   }
+    else if(Cfpw!=PW){
+
+        cfpwpara.innerHTML="Password not match"
+       
+        document.querySelector("#cfpw").value="";
+        document.querySelector("#cfpw").focus();
+         return false;
+   }
+   
+  
+
+
+}
